781d8ea05782a0c2622795dc63ecf43bf5a39fe2
turunenv <vili.turunen@aalto.fi>
2022-09-15 15:48:36 +0300

    13.16 - route api/authors returns aggregate info about authors

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -9,13 +9,15 @@ const { connectToDatabase } = require("./util/db");
 
 const blogRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
-const loginRouter = require("./controllers/login")
+const loginRouter = require("./controllers/login");
+const authorsRouter = require("./controllers/authors");
 
 app.use(express.json());
 
 app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/authors", authorsRouter);
 
 app.use(errorHandler);
 
