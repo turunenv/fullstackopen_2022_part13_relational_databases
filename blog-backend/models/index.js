0acb629bb5a205e83a51f982820d394449e9328d
turunenv <vili.turunen@aalto.fi>
2022-09-21 16:28:58 +0300

    13.19 - added migration for user_blogs connection table

--- blog-backend/models/index.js
+++ blog-backend/models/index.js
@@ -1,12 +1,17 @@
 const Blog = require("./blog");
 const User = require("./user");
+const UserBlogs = require("./userblogs");
 
 
 //add the one-to-many relationship between the database tables 'users' and 'blogs'
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
+User.belongsToMany(Blog, { through: UserBlogs, as: "marked_blogs" });
+Blog.belongsToMany(User, { through: UserBlogs, as: "users_marked" })
+
 module.exports = {
   Blog,
   User,
+  UserBlogs,
 };
